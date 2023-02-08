647e4307faa2ec8d80452530b6241e7415a8afb2
Анастасия Искоскина <aiskoskina@yandex.ru>
2023-02-08 15:07:59 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(requests):
+    return HttpResponse('Это главная страница')
+
+def group_posts(requests):
+    return HttpResponse('Список всех публикаций')
+
+def post_detail(requests, post_id):
+    return HttpResponse('Страница с одним постом {post_id}')
