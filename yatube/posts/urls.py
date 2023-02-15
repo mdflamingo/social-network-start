ece2e994f057578ea63d7419a66246e5a661c8d4
Анастасия Искоскина <aiskoskina@yandex.ru>
2023-02-15 12:05:09 +0300

    Отредактированы index  и group_list

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('posts/<slug:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/group/<slug>/', views.post_detail, name='post_detail'),
     path('posts/', views.group_posts, name='group_list'),
     path('', views.index, name='index'),
 ]
